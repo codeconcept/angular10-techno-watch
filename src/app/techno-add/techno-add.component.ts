adb3a92cb0ae441cbe577589cc0438422d9a20a1
codeconcept <contact@codeconcept.fr>
2020-11-03 13:27:04 +0100

    Form (template driven)

--- src/app/techno-add/techno-add.component.ts
+++ src/app/techno-add/techno-add.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-techno-add',
   templateUrl: './techno-add.component.html',
-  styleUrls: ['./techno-add.component.css']
+  styleUrls: ['./techno-add.component.css'],
 })
 export class TechnoAddComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addTechno(form) {
+    console.log(form.value);
   }
-
 }
